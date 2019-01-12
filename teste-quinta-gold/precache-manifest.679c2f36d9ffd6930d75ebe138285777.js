77d8b94272308fe971c6e96a879351be08a5ca6b
Jonas Mello <jonas@ekg.com.br>
2019-01-12 02:59:02 -0200

    Added teste-quinta-gold folder to test.

---
+++ teste-quinta-gold/precache-manifest.679c2f36d9ffd6930d75ebe138285777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb9fb4473f4791d7fef",
+    "url": "/teste-quinta-gold/static/js/runtime~main.dcb9fb44.js"
+  },
+  {
+    "revision": "abec6248b098b0d24a26",
+    "url": "/teste-quinta-gold/static/js/main.abec6248.chunk.js"
+  },
+  {
+    "revision": "a14ef0c0f3005f9ec1ec",
+    "url": "/teste-quinta-gold/static/js/1.a14ef0c0.chunk.js"
+  },
+  {
+    "revision": "abec6248b098b0d24a26",
+    "url": "/teste-quinta-gold/static/css/main.7f6c0ec9.chunk.css"
+  },
+  {
+    "revision": "0b0fd38b4987314c1582fde0cdaf8e50",
+    "url": "/teste-quinta-gold/index.html"
+  }
+];
--- teste-quinta-gold/service-worker.js
+++ teste-quinta-gold/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fadb3b6d1791ba3677e4d59864979b6.js"
+  "/teste-quinta-gold/precache-manifest.679c2f36d9ffd6930d75ebe138285777.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/teste-quinta-gold/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
