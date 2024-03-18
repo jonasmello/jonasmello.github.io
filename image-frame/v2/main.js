7b55117a1900ad88278eb2b715b0bce80fcef628
Jonas Mello <jonas@ekg.com.br>
2024-03-18 20:18:07 -0300

    v2 first test

---
+++ image-frame/v2/main.js
@@ -0,0 +1,75 @@
+const createElements = (src) => {
+  const $bg = document.createElement("img");
+  $bg.setAttribute("style", "display: none");
+  const $imgFromInput = document.createElement("img");
+  $imgFromInput.setAttribute("style", "display: none");
+  const $canvas = document.createElement("canvas");
+  $canvas.setAttribute("style", "display: none");
+  $imgFromInput.src = src;
+
+  return [$bg, $imgFromInput, $canvas];
+};
+
+const makeImage = (src) => {
+  [$bg, $imgFromInput, $canvas] = createElements(src);
+
+  $imgFromInput.onload = () => {
+    const ratio = $imgFromInput.width / $imgFromInput.height;
+
+    const isLandscape = $imgFromInput.width > $imgFromInput.height;
+    $bg.src = `./img/fondue.png?3`;
+
+    $bg.onload = () => {
+      const context = $canvas.getContext("2d");
+      $canvas.width = $bg.width;
+      $canvas.height = $bg.height;
+
+      const landscapeHeight = 1000;
+
+      const dimensions = isLandscape
+        ? [
+            1200 + (550 - landscapeHeight * ratio) / 2,
+            750,
+            landscapeHeight * ratio,
+            landscapeHeight,
+          ]
+        : [880, 750, 1150, 1150 / ratio];
+
+      context.globalAlpha = 1;
+      context.drawImage($imgFromInput, ...dimensions);
+      context.globalAlpha = 0.6;
+      context.drawImage($bg, 0, 0);
+
+      // context.globalAlpha = 1;
+      // context.drawImage($bg, 0, 0);
+      // context.globalAlpha = 0.5;
+      // context.drawImage($imgFromInput, ...dimensions);
+
+      const b64Img = $canvas.toDataURL("image/png");
+      const $result = document.createElement("img");
+      $result.src = b64Img;
+      $result.setAttribute("style", "max-width: 80vw; max-height: 80vh");
+
+      document.body.appendChild($result);
+    };
+  };
+};
+
+function changeEvent(e) {
+  if (e.target.files && e.target.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      makeImage(e.target.result);
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  }
+}
+
+(function () {
+  const $input = document.querySelector("input");
+
+  $input.addEventListener("change", changeEvent);
+
+  // makeImage("./tmp/paisagem.jpg");
+  // makeImage("./tmp/retrato.jpg");
+})();
